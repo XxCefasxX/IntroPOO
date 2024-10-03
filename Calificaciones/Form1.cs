852b7ede20ce5bf11e5c296545709f9ca7215ef2
Pedro Salazar <salazar9_5@hotmail.com>
2024-10-03 07:07:04 -0700

    calif

---
+++ Calificaciones/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calificaciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void VerMaterias(object sender, EventArgs e)
+        {
+            Materias _materias = new Materias();
+            _materias.Show();
+        }
+        private void VerAlumnos(object sender, EventArgs e)
+        {
+            Alumnos _alumnos = new Alumnos();
+            _alumnos.Show();
+        }
+
+
+    }
+}
