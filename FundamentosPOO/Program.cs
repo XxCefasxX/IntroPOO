3f1eb459353e13f2a1c294440c794f486c3248bc
Pedro Salazar <salazar9_5@hotmail.com>
2024-10-08 07:51:26 -0700

    herencia

---
+++ FundamentosPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona2 persona2 = 
+                new Persona2("Juan",
+                "juan@correo.com",
+                1);
+            Console.ReadLine();
+            persona2.Nombre = "Juan Luis";
+            persona2.Email = "jluis@correo.com";
+            persona2.ImprimeNombre();
+            persona2.ImprimeEmail();
+            persona2.CambiarFecha(DateTime.Now);
+            persona2.ImprimeFecha();
+            Console.ReadLine();
+        }
+    }
+}
